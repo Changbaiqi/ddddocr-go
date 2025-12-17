4d6ffc065aeba68f78daa1a242e828d6587429f0
Changbaiqi <changbaiqi@foxmail.com>
2025-12-17 17:53:51 +0800

    update: 更新支持android

--- utils/ocr_lib_android.go
+++ utils/ocr_lib_android.go
@@ -2,7 +2,9 @@
 
 package utils
 
+import "fmt"
+
 func init() {
-	fmt.Printf("触发GOOS:%v  GOARCH\n\n\n", runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("触发GOOS:%v  GOARCH:%v\n\n\n", runtime.GOOS, runtime.GOARCH)
 	sharedLibName = "onnxruntime_android_arm64.so"
 }
