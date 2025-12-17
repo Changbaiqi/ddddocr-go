c23afb5d22c32047e0ca63964ee27a0327c3c214
Changbaiqi <changbaiqi@foxmail.com>
2025-12-17 16:59:33 +0800

    update: 更新支持android

---
+++ utils/ocr_lib_android.go
@@ -0,0 +1,16 @@
+//go:build android
+
+package utils
+
+//go:embed assets/onnxruntime_android_arm64.so
+//go:embed assets/common_old.onnx
+//go:embed assets/common_tencent.onnx
+//go:embed assets/calc_det.onnx
+var assets embed.FS
+
+func init() {
+	switch runtime.GOARCH {
+	case "arm64":
+		sharedLibName = "onnxruntime_android_arm64.so0"
+	}
+}
